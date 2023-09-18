51ebdb20e7f22a114a51869284f3068d0581a57d
Michael Canova <mcanova@usgs.gov>
2023-09-18 12:21:09 -0500

    Create documentation for lib_summary

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,12 @@
+#' R Library Summary
+#'
+#' Provides a brief summary of the packages installed on your machine.
+#'
+#' @return A `data.frame` containing a count of the packages in each of the user's
+#'  libraries.
+#' @export
+#'
+#' @examples
 lib_summary <- function(){
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
